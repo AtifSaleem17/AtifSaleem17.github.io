f8834f3daaf8b99d40be7cab64ce459a7f98a43b
Muhammad Atif <67201977+AtifSaleem17@users.noreply.github.com>
2023-08-30 18:57:27 +0100

    Add files via upload

---
+++ styro/active.js
@@ -0,0 +1,56 @@
+
+  //object references
+  var a1=document.getElementById("a1");
+  var a2=document.getElementById("a2");
+  var a3=document.getElementById("a3");
+  var a4=document.getElementById("a4");
+  var a5=document.getElementById("a5");
+  var a6=document.getElementById("a6");
+  
+  //events handling for a1
+  a1.addEventListener("focus",function(){
+    a1.style.borderRight="5px solid";
+  });
+  a1.addEventListener("blur", function(){
+    a1.style.borderRight="1px solid";
+  });
+
+    //events handling for a2
+  a2.addEventListener("focus",function(){
+    a2.style.borderRight="5px solid";
+  });
+  a2.addEventListener("blur", function(){
+    a2.style.borderRight="1px solid";
+  });
+
+    //events handling for a3
+  a3.addEventListener("focus",function(){
+    a3.style.borderRight="5px solid";
+  });
+  a3.addEventListener("blur", function(){
+    a3.style.borderRight="1px solid";
+  });
+
+    //events handling for a4
+  a4.addEventListener("focus",function(){
+    a4.style.borderRight="5px solid";
+  });
+  a4.addEventListener("blur", function(){
+    a4.style.borderRight="1px solid";
+  });
+
+    //events handling for a5
+  a5.addEventListener("focus",function(){
+    a5.style.borderRight="5px solid";
+  });
+  a5.addEventListener("blur", function(){
+    a5.style.borderRight="1px solid";
+  });
+
+    //events handling for a6
+  a6.addEventListener("focus",function(){
+    a6.style.borderRight="5px solid";
+  });
+  a6.addEventListener("blur", function(){
+    a6.style.borderRight="1px solid";
+  });
